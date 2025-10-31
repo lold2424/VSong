0145aba65794de14e1b12c8e8979a6d5a2ba0555
lold2424 <lold2424@gmail.com>
2025-10-31 18:34:18 +0900

    feat: 배포 환경 변수 추가

--- src/front/vite.config.js
+++ src/front/vite.config.js
@@ -12,6 +12,10 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
+      '/oauth2': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
     },
   },
 })
