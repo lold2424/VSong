09844e57c478a3ed1783215b80c2594858be376d
lold2424 <lold2424@gmail.com>
2025-11-19 00:04:07 +0900

    fix: PM2 설정에 cwd를 추가

--- front-next/ecosystem.config.js
+++ front-next/ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: 'vsong-frontend',
       script: 'server.js',
+      cwd: '/home/opc/vsong/frontend',
       instances: 1,
       autorestart: true,
       watch: false,
