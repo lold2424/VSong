d0cc30335d4a4860937dbdaee67fb7e248485af0
lold2424 <lold2424@gmail.com>
2025-12-01 16:52:29 +0900

    fix: csp를 통해 비디오 모달에 유튜브 임베드 허용

--- front-next/next.config.js
+++ front-next/next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: img.youtube.com yt3.ggpht.com lh3.googleusercontent.com; object-src 'none'; frame-ancestors 'self';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: img.youtube.com yt3.ggpht.com lh3.googleusercontent.com; object-src 'none'; frame-ancestors 'self'; frame-src https://www.youtube.com;",
           },
         ],
       },
